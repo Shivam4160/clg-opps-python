c0aafc88319d9de96a3bdcb351f4c457eda5a752
Shivam Tejani <sq.shivam4106@gmail.com>
2024-12-21 16:39:16 +0530

    Add files via upload
    21-12-2024

---
+++ Shivam Tejani (CS)/OPPS/student.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class student{
+	int rno;
+	char name[50];
+	double fee;
+	public:
+		student()
+		{
+			cout<<"Enter roll no:";
+			cin>>rno;
+			cout<<"Enter name:";
+			cin>>name;
+			cout<<"Enter roll fee:";
+			cin>>fee;
+		}
+		
+		void display();
+};
+
+void student :: display()
+{
+	cout<<endl<<rno<<"\t"<<name<<"\t"<<fee;
+}
+
+int main()
+{
+	student s;
+	s.display();
+	return 0;
+}
